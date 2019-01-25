a1829fc40aff16a60b18a263dff5adb76b227645
Designburo <info@designburo.nl>
2019-01-25 16:02:58 +0100

    v 0.1.7 : Show top visited pages with date range. Show as csv option

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # WSStats
 This MediaWiki extension counts pageviews by user
 
+* Version 0.1.7 : Show top visited pages with date range. Show as csv option
 * Version 0.1.6 : Filter results on user or anonymous
 * Version 0.1.5 : Added more configuration options
 * Version 0.1.3 : Fixed error in MySQL
@@ -56,20 +57,22 @@ $wgWSStats['ignore_in_url'][] = 'Template:Test';
 $wgWSStats['ignore_in_url'][] = 'action=edit';
 ````
 
+#Usage
+
 === Ask number of hits for page id : 9868 ===
 
 ```
 {{#wsstats:id=9868}}
 ```
 
-=== Ask number of hits for page id : 714 (homepage) since start date 2018-02-01 ===
+=== Ask number of hits for page id : 714 since start date 2018-02-01 ===
 
 ```
 {{#wsstats:id=714
 |start date=2018-02-01}}
 ```
 
-=== Ask number of hits for page id : 714 (homepage) since start date 2018-02-01 and end date 2018-02-14 ===
+=== Ask number of hits for page id : 714 since start date 2018-02-01 and end date 2018-02-14 ===
 
 ```
 {{#wsstats:id=714
@@ -91,4 +94,25 @@ $wgWSStats['ignore_in_url'][] = 'action=edit';
 |start date=2018-02-01
 |end date=2018-02-08
 |type=only user}}
+```
+
+=== Get the top ten pages sorted by hits ===
+
+```
+{{#wsstats:stats}}
+```
+
+=== Get the top ten pages sorted by hits in a date range ===
+
+```
+{{#wsstats:stats
+|start date=2018-02-01
+|end date=2018-02-08}}
+```
+
+=== Get the top ten pages sorted by hits and show as csv ===
+
+```
+{{#wsstats:stats
+|format:csv}}
 ```
--- WSStats.hooks.php
+++ WSStats.hooks.php
@@ -7,8 +7,8 @@
  *
  */
 
-if (!defined('MEDIAWIKI')) {
-	die('This file is a MediaWiki extension, it is not a valid entry point');
+if ( ! defined( 'MEDIAWIKI' ) ) {
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
 class WSStatsHooks {
@@ -16,235 +16,288 @@ class WSStatsHooks {
 	public static $db_prefix = "";
 
 	public function __construct() {
-		date_default_timezone_set('UTC');
+		date_default_timezone_set( 'UTC' );
 		global $wgDBprefix;
 		WSStatsHooks::$db_prefix = $wgDBprefix;
 	}
 
 	public static function isAnon() {
 		return User::isAnon();
-}
+	}
 
-public static function db_open() {
+	public static function db_open() {
 		global $wgDBserver;
 		global $wgDBname;
 		global $wgDBuser;
 		global $wgDBpassword;
 
-		$conn = new MySQLi($wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname);
-		$conn->set_charset("utf8");
+		$conn = new MySQLi( $wgDBserver, $wgDBuser, $wgDBpassword, $wgDBname );
+		$conn->set_charset( "utf8" );
+
 		return $conn;
-}
-
-public static function db_real_escape($txt) {
-		$db = WSssdHooks::db_open();
-		$txt = $db->real_escape_string($txt);
-		$db->close();
-		return $txt;
-
-}
-
-public static function getPageTitleFromID($id) {
-	$artikel = Article::newFromId($id);
-	$title = $artikel->getTitle();
-	$furl = $title->getFullText();
-	return $furl;
-}
-
-
-/**
- * When running maintenance update with will add the database tables
- * @param [type] $updater [description]
- */
-	public static function addTables($updater) {
-			$dbt = $updater->getDB()->getType();
-			// If using SQLite, just use the MySQL/MariaDB schema, it's compatible
-			// anyway. Only PGSQL and some more exotic variants need a totally
-			// different schema.
-			if ($dbt === 'sqlite') {
-					$dbt = 'sql';
-			}
-			$tables = __DIR__ . "/WSStats.$dbt";
-
-			if (file_exists($tables)) {
-					$updater->addExtensionUpdate(array('addTable', 'WSPS', $tables, true));
-			} else {
-					throw new MWException("WSStats does not support $dbt.");
-			}
-			return true;
 	}
 
-	public static function getViewsPerPage($id,$dates=false, $type = false) {
-	    if($type === 'only anonymous') {
-	        $type = " AND user_id = 0 ";
-        }
-        if($type === 'only user') {
-            $type = " AND user_id <> 0 ";
-        }
-        if( $type === false ) {
-            $type = '';
-        }
-		if( $dates === false ) {
-			$sql = 'SELECT page_id, COUNT(*) as count from '.WSStatsHooks::$db_prefix.'WSPS WHERE page_id=\''.$id.'\' '.$type.'GROUP BY page_id ORDER BY count DESC limit 1';
+	public static function db_real_escape( $txt ) {
+		$db  = WSssdHooks::db_open();
+		$txt = $db->real_escape_string( $txt );
+		$db->close();
+
+		return $txt;
+
+	}
+
+	public static function getPageTitleFromID( $id ) {
+		$artikel = Article::newFromId( $id );
+		$title   = $artikel->getTitle();
+		$furl    = $title->getFullText();
+
+		return $furl;
+	}
+
+
+	/**
+	 * When running maintenance update with will add the database tables
+	 *
+	 * @param [type] $updater [description]
+	 */
+	public static function addTables( $updater ) {
+		$dbt = $updater->getDB()->getType();
+		// If using SQLite, just use the MySQL/MariaDB schema, it's compatible
+		// anyway. Only PGSQL and some more exotic variants need a totally
+		// different schema.
+		if ( $dbt === 'sqlite' ) {
+			$dbt = 'sql';
+		}
+		$tables = __DIR__ . "/WSStats.$dbt";
+
+		if ( file_exists( $tables ) ) {
+			$updater->addExtensionUpdate( array( 'addTable', 'WSPS', $tables, true ) );
 		} else {
-			if ($dates['e'] === false) {
-				$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE page_id=\'' . $id . '\' '.$type.'AND added BETWEEN \'' . $dates["b"] . '\' AND NOW()';
+			throw new MWException( "WSStats does not support $dbt." );
+		}
+
+		return true;
+	}
+
+	public static function getViewsPerPage( $id, $dates = false, $type = false ) {
+		if ( $type === 'only anonymous' ) {
+			$type = " AND user_id = 0 ";
+		}
+		if ( $type === 'only user' ) {
+			$type = " AND user_id <> 0 ";
+		}
+		if ( $type === false ) {
+			$type = '';
+		}
+		if ( $dates === false ) {
+			$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE page_id=\'' . $id . '\' ' . $type . 'GROUP BY page_id ORDER BY count DESC LIMIT 1';
+		} else {
+			if ( $dates['e'] === false ) {
+				$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE page_id=\'' . $id . '\' ' . $type . 'AND added BETWEEN \'' . $dates["b"] . '\' AND NOW()';
 			} else {
-				$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE page_id=\'' . $id . '\' '.$type.'AND added >= \'' . $dates["b"] . '\' AND added <= \'' . $dates['e'] . '\' GROUP BY page_id ORDER BY COUNT DESC LIMIT 1';
+				$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE page_id=\'' . $id . '\' ' . $type . 'AND added >= \'' . $dates["b"] . '\' AND added <= \'' . $dates['e'] . '\' GROUP BY page_id ORDER BY COUNT DESC LIMIT 1';
 			}
 		}
 
 		$db = WSStatsHooks::db_open();
-		$q=$db->query($sql);
-		if( $q === false ) {
+		$q  = $db->query( $sql );
+		if ( $q === false ) {
 			return 0;
 		}
 		$row = $q->fetch_assoc();
-		if(!isset($row['count']) || empty($row['count'])) {
+		if ( ! isset( $row['count'] ) || empty( $row['count'] ) ) {
 			return 0;
-		} else return $row['count'];
-	}
-
-	public static function getMostViewedPages($dates=false) {
-		$sql = 'SELECT page_id, COUNT(*) as count from '.WSStatsHooks::$db_prefix.'WSPS GROUP BY page_id ORDER BY count DESC limit 10';
-		$db = WSStatsHooks::db_open();
-	    $q=$db->query($sql);
-	    if ( $q->num_rows > 0 ) {
-			$data = "{| class=\"sortable wikitable smwtable jquery-tablesorter\"\n";
-			$data .= "! Page ID\n";
-			$data .= "! Page Title\n";
-			$data .= "! Page hits\n";
-			while ($row = $q->fetch_assoc()) {
-				$data .= "|-\n";
-		  	$data .= "| " . $row['page_id'] . "\n";
-				$data .= "| " . WSStatsHooks::getPageTitleFromID($row['page_id']) . "\n";
-				$data .= "| " . $row['count'] . "\n";
-			  $data .= "|-\n";
-			}
-		$data .= "|}\n";
-		$db->close();
-		return $data;
+		} else {
+			return $row['count'];
 		}
-}
-
-	public static function getOptionSetting($options, $k) {
-		if ( isset( $options[$k] ) && $options[$k] != '' ) {
-			return $options[$k];
-		} else return false;
 	}
 
-	public static function onParserFirstCallInit(Parser &$parser) {
-			$parser->setFunctionHook('wsstats', 'WSStatsHooks::wsstats');
+	public static function getMostViewedPages( $dates = false, $render = "table" ) {
+
+		if ( $dates === false ) {
+			$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS GROUP BY page_id ORDER BY count DESC LIMIT 10';
+		} else {
+			if ( $dates['e'] === false ) {
+				$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE added BETWEEN \'' . $dates["b"] . '\' AND NOW() GROUP BY page_id ORDER BY count DESC LIMIT 10';
+			} else {
+				$sql = 'SELECT page_id, COUNT(*) AS count FROM ' . WSStatsHooks::$db_prefix . 'WSPS WHERE added >= \'' . $dates["b"] . '\' AND added <= \'' . $dates['e'] . '\' GROUP BY page_id ORDER BY COUNT DESC LIMIT 10';
+			}
+		}
+
+		$db = WSStatsHooks::db_open();
+		$q  = $db->query( $sql );
+		if ( $q->num_rows > 0 ) {
+			if ( $render === 'table' ) {
+				$data = "{| class=\"sortable wikitable smwtable jquery-tablesorter\"\n";
+				$data .= "! Page ID\n";
+				$data .= "! Page Title\n";
+				$data .= "! Page hits\n";
+				while ( $row = $q->fetch_assoc() ) {
+					$data .= "|-\n";
+					$data .= "| " . $row['page_id'] . "\n";
+					$data .= "| " . WSStatsHooks::getPageTitleFromID( $row['page_id'] ) . "\n";
+					$data .= "| " . $row['count'] . "\n";
+					$data .= "|-\n";
+				}
+				$data .= "|}\n";
+				$db->close();
+
+				return $data;
+			}
+			if ( $render === 'csv' ) {
+				$data = '';
+				while ( $row = $q->fetch_assoc() ) {
+					$data .= $row['page_id'] . ";" . $row['count'] . ",";
+				}
+				$data = rtrim( $data, ',' );
+				$db->close();
+
+				return $data;
+			}
+		}
 	}
 
+	public static function getOptionSetting( $options, $k ) {
+		if ( isset( $options[ $k ] ) && $options[ $k ] != '' ) {
+			return $options[ $k ];
+		} else {
+			return false;
+		}
+	}
+
+	public static function onParserFirstCallInit( Parser &$parser ) {
+		$parser->setFunctionHook( 'wsstats', 'WSStatsHooks::wsstats' );
+	}
 
 
 	public static function onBeforePageDisplay( outputPage &$output, Skin &$skin ) {
 		global $wgUser, $wgWSStats;
 
-		if( isset( $_SERVER['HTTP_REFERER'] ) ) {
-            $ref = $_SERVER['HTTP_REFERER'];
-        } else $ref = false;
+		if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
+			$ref = $_SERVER['HTTP_REFERER'];
+		} else {
+			$ref = false;
+		}
 
 
+		if ( $wgWSStats['count_all_usergroups'] !== true ) {
 
-		if( $wgWSStats['count_all_usergroups'] !== true ) {
+			if ( isset( $wgWSStats['skip_user_groups'] ) && is_array( $wgWSStats['skip_user_groups'] ) ) {
+				$groups = $wgWSStats['skip_user_groups'];
+				foreach ( $groups as $group ) {
+					if ( in_array( $group, $wgUser->getGroups() ) ) {
+						return true;
+					}
+				}
+			}
+		}
 
-            if (isset($wgWSStats['skip_user_groups']) && is_array($wgWSStats['skip_user_groups'])) {
-                $groups = $wgWSStats['skip_user_groups'];
-                foreach ($groups as $group) {
-                    if (in_array($group, $wgUser->getGroups())) {
-                        return true;
-                    }
-                }
-            }
-        }
+		if ( isset( $wgWSStats['ignore_in_url'] ) && is_array( $wgWSStats['ignore_in_url'] ) && $ref !== false ) {
+			$ignore = $groups = $wgWSStats['ignore_in_url'];
+			foreach ( $ignore as $single ) {
+				if ( strpos( $ref, $single ) !== false ) {
+					return true;
+				}
+			}
+		}
 
-        if ( isset( $wgWSStats['ignore_in_url'] ) && is_array( $wgWSStats['ignore_in_url'] ) && $ref !== false ) {
-            $ignore = $groups = $wgWSStats['ignore_in_url'];
-            foreach( $ignore as $single ) {
-                if( strpos($ref, $single) !== false ) {
-                    return true;
-                }
-            }
-        }
+		if ( isset( $wgWSStats['skip_anonymous'] ) && $wgWSStats['skip_anonymous'] === true ) {
+			if ( $wgUser->isAnon() ) {
+				return true;
+			}
 
-		if( isset($wgWSStats['skip_anonymous']) && $wgWSStats['skip_anonymous'] === true) {
-            if($wgUser->isAnon()) {
-                return true;
-            }
+		}
 
-        }
-
-		if($wgUser->isAnon()) {
-			$data['user_id']=0;
+		if ( $wgUser->isAnon() ) {
+			$data['user_id'] = 0;
 		} else {
 			$data['user_id'] = $wgUser->getID();
 		}
 		$title = $output->getTitle();
 
-		if($title===null) return true;
-		$data['page_id']=$title->getArticleID();
-		if($data['page_id'] != 0) {
-			WSStatsHooks::insertRecord('WSPS', $data);
+		if ( $title === null ) {
+			return true;
 		}
-		 return true;
+		$data['page_id'] = $title->getArticleID();
+		if ( $data['page_id'] != 0 ) {
+			WSStatsHooks::insertRecord( 'WSPS', $data );
+		}
+
+		return true;
 
 	}
 
-	public static function wsstats(Parser &$parser) {
-		//Not implemented yet
+	public static function wsstats( Parser &$parser ) {
 		$options = WSStatsHooks::extractOptions( array_slice( func_get_args(), 1 ) );
 		if ( isset( $options['stats'] ) ) {
-			$data = WSStatsHooks::getMostViewedPages();
-			return $data;
-		}
-		$pid = WSStatsHooks::getOptionSetting($options,'id');
-		if ( $pid !== false ) {
-            $type = WSStatsHooks::getOptionSetting($options,'type');
-			$dates = array();
-			$dates['b'] = WSStatsHooks::getOptionSetting($options,'start date');
-			$dates['e'] = WSStatsHooks::getOptionSetting($options,'end date');
-			if ($dates['e'] === false && $dates['b'] !== false ) {
+			$dates  = array();
+			$format = WSStatsHooks::getOptionSetting( $options, 'format' );
+			if ( $format === false ) {
+				$format = 'table';
+			}
+			$dates['b'] = WSStatsHooks::getOptionSetting( $options, 'start date' );
+			$dates['e'] = WSStatsHooks::getOptionSetting( $options, 'end date' );
+			if ( $dates['e'] === false && $dates['b'] !== false ) {
 				$dates['e'] = false;
 			}
-			if ($dates['b'] === false && $dates['e'] !== false ) {
+			if ( $dates['b'] === false && $dates['e'] !== false ) {
 				$dates = false;
 			}
-			if ($dates['b'] === false && $dates['e'] === false ) {
+			if ( $dates['b'] === false && $dates['e'] === false ) {
 				$dates = false;
 			}
-			$data = WSStatsHooks::getViewsPerPage($pid,$dates,$type);
-			if($data !== NULL) {
-				return $data;
-			} else return "";
+			$data = WSStatsHooks::getMostViewedPages( $dates, $format );
+
+			return $data;
 		}
+		$pid = WSStatsHooks::getOptionSetting( $options, 'id' );
+		if ( $pid !== false ) {
+			$type       = WSStatsHooks::getOptionSetting( $options, 'type' );
+			$dates      = array();
+			$dates['b'] = WSStatsHooks::getOptionSetting( $options, 'start date' );
+			$dates['e'] = WSStatsHooks::getOptionSetting( $options, 'end date' );
+			if ( $dates['e'] === false && $dates['b'] !== false ) {
+				$dates['e'] = false;
+			}
+			if ( $dates['b'] === false && $dates['e'] !== false ) {
+				$dates = false;
+			}
+			if ( $dates['b'] === false && $dates['e'] === false ) {
+				$dates = false;
+			}
+			$data = WSStatsHooks::getViewsPerPage( $pid, $dates, $type );
+			if ( $data !== null ) {
+				return $data;
+			} else {
+				return "";
+			}
+		}
+
 		return "ok, move along. Nothing to see here..";
 	}
 
 
-	public static function insertRecord($table, $vals) {
-			$dbw = wfGetDB(DB_MASTER);
-			$dbw->IngoreErrors = true;
-			try {
-					$res = $dbw->insert(
-							$table,
-							$vals,
-							__METHOD__
-					);
-			} catch (Exception $e) {
-					echo $e;
-					return false;
-			}
+	public static function insertRecord( $table, $vals ) {
+		$dbw               = wfGetDB( DB_MASTER );
+		$dbw->IngoreErrors = true;
+		try {
+			$res = $dbw->insert(
+				$table,
+				$vals,
+				__METHOD__
+			);
+		} catch ( Exception $e ) {
+			echo $e;
 
-			if ($res) {
-					return true;
-			} else {
-					return false;
-			}
+			return false;
+		}
+
+		if ( $res ) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-
-
 
 
 	/**
@@ -253,34 +306,35 @@ public static function getPageTitleFromID($id) {
 	 * true is assumed like this: [name] => true
 	 *
 	 * @param array string $options
+	 *
 	 * @return array $results
 	 */
-	public static function extractOptions(array $options) {
-			$results = array();
-			foreach ($options as $option) {
-					$pair = explode('=', $option, 2);
-					if ($pair[0] !== '//') {
-							if (count($pair) === 2) {
-									$name = strtolower(trim($pair[0]));
-									if ($name=='template') {
-										$value = trim($pair[1]);
-									} else {
-										$value = strtolower(trim($pair[1]));
-									}
-
-									$results[$name] = $value;
-							}
-							if (count($pair) === 1) {
-									$name = trim($pair[0]);
-									$results[$name] = true;
-							}
+	public static function extractOptions( array $options ) {
+		$results = array();
+		foreach ( $options as $option ) {
+			$pair = explode( '=', $option, 2 );
+			if ( $pair[0] !== '//' ) {
+				if ( count( $pair ) === 2 ) {
+					$name = strtolower( trim( $pair[0] ) );
+					if ( $name == 'template' ) {
+						$value = trim( $pair[1] );
+					} else {
+						$value = strtolower( trim( $pair[1] ) );
 					}
+
+					$results[ $name ] = $value;
+				}
+				if ( count( $pair ) === 1 ) {
+					$name             = trim( $pair[0] );
+					$results[ $name ] = true;
+				}
 			}
-			//Now you've got an array that looks like this:
-			//  [foo] => "bar"
-			//	[apple] => "orange"
-			//	[banana] => true
-			return $results;
+		}
+		//Now you've got an array that looks like this:
+		//  [foo] => "bar"
+		//	[apple] => "orange"
+		//	[banana] => true
+		return $results;
 	}
 
 }
